31bde58e3b17b9fe984a8cf6cafd868884774d10
George Pamfilis <gpamfilis@gmail.com>
2017-08-31 17:21:35 +0300

    pull request

--- app/main/views.py
+++ app/main/views.py
@@ -5,4 +5,4 @@ from . import main
 @main.route('/')
 def home():
     """Render website's home page."""
-    return 'hey motherfucker i weant youddddd' #render_template('about.html')
+    return 'hey motherfucker i weantdfsdsddd' #render_template('about.html')
