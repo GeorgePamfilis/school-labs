be8ed405579db308c8c56c5ed0819a9784b6578c
George Pamfilis <gpamfilis@gmail.com>
2017-08-31 17:38:34 +0300

    pull request on branch pennis

--- app/main/views.py
+++ app/main/views.py
@@ -5,4 +5,4 @@ from . import main
 @main.route('/')
 def home():
     """Render website's home page."""
-    return 'hey motherfucker i weantdf pennis' #render_template('about.html')
+    return 'hey motherfucker' #render_template('about.html')
