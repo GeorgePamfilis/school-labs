6a39b1a6d032be03cf32b1f262a1b37089b8d909
gpamfilis <gpamfilis@gmail.com>
2017-11-06 12:58:59 +0200

    testing new

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask_bootstrap import Bootstrap
-# from flask_login import LoginManager
+from flask_login import LoginManager
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
@@ -7,12 +7,13 @@ from config import config
 db = SQLAlchemy()
 
 
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 csrf = CSRFProtect()
 bootstrap = Bootstrap()
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -21,11 +22,10 @@ def create_app(config_name):
     csrf.init_app(app)
     db.init_app(app)
     bootstrap.init_app(app)
-    # login_manager.init_app(app)
+    login_manager.init_app(app)
 
     # TODO: organize the blueprints, rename
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint, url_prefix='')
 
     return app
-
