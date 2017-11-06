d4e29bf601a72ee9408e7f29911b407d2b995b15
gpamfilis <gpamfilis@gmail.com>
2017-11-06 12:39:18 +0200

    testing new

--- app/main/views.py
+++ app/main/views.py
@@ -5,19 +5,11 @@ import os
 @main.route('/')
 def index():
     fs = os.listdir('./app/templates')
-    print fs
     """Render website's home page."""
     return render_template('index.html')
 
 
 @main.route('/notebooks/<path:loc>')
 def home(loc):
-    print loc
     """Render website's home page."""
     return render_template(loc)
-
-
-
-
-
-
